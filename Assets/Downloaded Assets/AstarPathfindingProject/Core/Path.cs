c088a528fd3277869f7849ce195a8eb166918a99
Mike Roark <mroark@techni-dynamics.com>
2015-11-30 15:03:48 -0700

    Committing changes to base.

--- Assets/Downloaded Assets/AstarPathfindingProject/Core/Path.cs
+++ Assets/Downloaded Assets/AstarPathfindingProject/Core/Path.cs
@@ -325,7 +325,7 @@ yield return StartCoroutine (p.WaitForPath ());
 			}
 
 			if (AstarPath.active.logPathResults != PathLog.None && AstarPath.active.logPathResults != PathLog.InGame) {
-				//Debug.LogWarning (msg);
+				Debug.LogWarning (msg);
 			}
 		}
 
